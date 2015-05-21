818f1d67c52c1a1614f9a6eb2ca1458ee09d0c6d
mihai <mstarniciuc@gmail.com>
2015-05-21 11:00:10 +0300

    add links to section

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,9 @@
 
+function goScroll(a) {
+    if (a != 0) {
+        $('html, body').animate({scrollTop: $("#" + a).offset().top}, 1000);
+    } else {
+        $('html, body').animate({scrollTop: 0}, 1000);
+    }
+
+}
